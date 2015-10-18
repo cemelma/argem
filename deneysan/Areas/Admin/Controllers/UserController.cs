a185cee10208575becbaec7e99cf099fbb4afeb3
Cem Elma <cemelma.20@gmail.com>
2015-10-18 13:05:02 +0300

    istekler

---
+++ deneysan/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using deneysan.Areas.Admin.Filters;
+using deneysan_BLL.ContactBL;
+using deneysan_BLL.LanguageBL;
+using deneysan_DAL.Entities;
+using deneysan_BLL.UserBL;
+
+namespace deneysan.Areas.Admin.Controllers
+{
+    [AuthenticateUser]
+    public class UserController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            var list = UserManager.UserList();
+            return View(list);
+        }
+
+        [AllowAnonymous]
+        public JsonResult DeleteRecord(int id)
+        {
+            bool isdelete = UserManager.UserDelete(id);
+            return Json(isdelete);
+        }
+
+    }
+}
